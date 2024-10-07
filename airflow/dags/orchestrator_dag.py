eec00a045005e80cfbdd75918f132250c487885a
Yago Mouro <ext.yago.mouro@agrogalaxy.com.br>
2024-10-07 17:11:49 -0300

    refactor: update  orchestrator dag script to use class-based structure

--- airflow/dags/orchestrator_dag.py
+++ airflow/dags/orchestrator_dag.py
@@ -1,60 +1,76 @@
 from airflow import DAG
-from airflow.providers.apache.spark.operators.spark_submit import SparkSubmitOperator
-from airflow.utils.task_group import TaskGroup
 from airflow.operators.dummy import DummyOperator
+from airflow.providers.apache.spark.operators.spark_submit import SparkSubmitOperator
+from airflow.utils.dates import days_ago
+from airflow.utils.task_group import TaskGroup
 from datetime import timedelta
+from airflow.decorators import dag
 
 default_args = {
     'owner': 'Yago Mouro',
     'depends_on_past': False,
     'email_on_failure': False,
     'email_on_retry': False,
-    'retries': 1,
-    'retry_delay': timedelta(seconds=10),
+    'retries': 2,
+    'retry_delay': timedelta(minutes=5),
 }
 
-with DAG(
-    'spark_jobs_orchestration',
+
+class SparkSubmitTask:
+    """Returns a SparkSubmitOperator with the provided task_id, application, and other configurations"""
+
+    def __init__(self, task_id, application, conn_id='spark_connection', retries=3, retry_delay=timedelta(minutes=2)):
+        self.task_id = task_id
+        self.application = application
+        self.conn_id = conn_id
+        self.retries = retries
+        self.retry_delay = retry_delay
+
+    def get_task(self):
+        return SparkSubmitOperator(
+            task_id=self.task_id,
+            application=self.application,
+            conn_id=self.conn_id,
+            verbose=True,
+            retries=self.retries,
+            retry_delay=self.retry_delay
+        )
+
+
+@dag(
+    dag_id='breweries_dags_orchestration',
     default_args=default_args,
-    description='DAG to orchestrate breweries data processing',
-    tags=['spark', 'ETL']
-) as dag:
+    schedule_interval=None,
+    catchup=False,
+    start_date=days_ago(1),
+    tags=['spark', 'etl', 'azure', 'datalake_storage']
+)
+def spark_jobs_orchestration():
+    start_task = DummyOperator(task_id='start')
 
-    start = DummyOperator(
-        task_id='start',
-        dag=dag
-    )
+    with TaskGroup('datalake_spark_jobs') as spark_jobs:
 
-    with TaskGroup('datalake_spark_jobs', dag=dag) as spark_jobs:
-
-        bronze_layer = SparkSubmitOperator(
-            application='/opt/airflow/jobs/bronze_layer.py',
-            conn_id='spark_connection',
+        bronze_layer = SparkSubmitTask(
             task_id='bronze_layer',
-            dag=dag
-        )
-        
-        silver_layer = SparkSubmitOperator(
-            task_id='silver_layer',
-            application='/opt/airflow/jobs/silver_layer.py',
-            conn_id='spark_connection',
-            verbose=True,
-            dag=dag
-        )
+            application='/opt/airflow/jobs/bronze_layer.py'
+        ).get_task()
 
-        gold_layer = SparkSubmitOperator(
+        silver_layer = SparkSubmitTask(
+            task_id='silver_layer',
+            application='/opt/airflow/jobs/silver_layer.py'
+        ).get_task()
+
+        gold_layer = SparkSubmitTask(
             task_id='gold_layer',
-            application='/opt/airflow/jobs/gold_layer.py',
-            conn_id='spark_connection',
-            verbose=True,
-            dag=dag
-        )
+            application='/opt/airflow/jobs/gold_layer.py'
+        ).get_task()
 
         bronze_layer >> silver_layer >> gold_layer
+        bronze_layer
 
-    end = DummyOperator(
-        task_id='end',
-        dag=dag
-    )
+    end_task = DummyOperator(task_id='end')
 
-    start >> spark_jobs >> end
+    start_task >> spark_jobs >> end_task
+
+
+dag_instance = spark_jobs_orchestration()
